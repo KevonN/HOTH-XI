a8ab8facf0f210cc2e5888c22a3094c0a9861515
sophzh <154521167+sophzh@users.noreply.github.com>
2024-03-03 15:46:07 -0800

    Changed input box appearance

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,60 +1,7 @@
-/*import React, { Component } from "react";
-
-class FormsAndInputs extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            word: ''
-        }
-        this.inputRef = React.createRef()
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const data = this.state
-    }
-
-    handleInputChange = (event) => {
-        event.preventDefault()
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleClearClick = (event) => {
-        console.log(this.inputRef.current.value)
-        this.setState({
-            word: ''
-        })
-    }
-
-    render () {
-        const {word} = this.state
-        return (
-            <div>
-                <p>Input is: {word}</p>
-                <form onSubmit={this.handleSubmit}>
-                    <p><input ref={this.inputRef} placeholder='type...' value={word} onChange={this.handleInputChange} name='word' /></p>
-                    <p><button onClick={this.handleClearClick}>Enter!</button></p>
-                </form>
-            </div>
-        )
-    }
-}
-
-// function InputBox (props) {
-
-//     return (
-//         <p>
-//             <input type='word' value={props.value} name={props.name} ref={props.ref} />
-//         </p>
-//     )
-// }
-
-export default FormsAndInputs; */
-
 // InputBox.jsx
 import React, { Component } from "react";
+
+import './InputBox.css'
 
 class FormsAndInputs extends Component {
     state = {
@@ -84,14 +31,15 @@ class FormsAndInputs extends Component {
         const { word } = this.state;
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
+                <form id='userForm' onSubmit={this.handleSubmit}>
                     <input
+                        id='userInputForm'
                         placeholder='type...'
                         value={word}
                         onChange={this.handleInputChange}
                         name='word'
                     />
-                    <button type="submit">Enter!</button>
+                    <button id='submitButton' type="submit">Enter!</button>
                 </form>
             </div>
         );
